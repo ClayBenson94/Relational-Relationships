61313a109c00f06fd8f8b8b8a96d17f0dd0c71aa
ClayBenson94 <cmb3602@g.rit.edu>
2016-02-23 12:38:18 -0500

    Added H2 Demo files

--- src/RelationalRelationships.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Clay on 2/23/2016.
- */
-public class RelationalRelationships {
-    public static void main(String[] args) {
-        System.out.println("Hello fellow daters! ;)");
-    }
-}
