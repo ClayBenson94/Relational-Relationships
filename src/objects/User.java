82bd9a51209e3d89afe8b770ca052cbe837000f3
ClayBenson94 <cmb3602@g.rit.edu>
2016-03-15 13:28:26 -0400

    Added basic user object with nothing really in it besides member variables

---
+++ src/objects/User.java
@@ -0,0 +1,18 @@
+package objects;
+
+/**
+ * @author Clay Benson cmb3602@g.rit.edu
+ */
+public class User {
+
+    private String username;
+    private String password;
+    private String name;
+    private String bio;
+    private String email;
+    private long dob;
+//    private ENUM gender;
+//    private ENUM sexuality;
+    private int location;
+
+}
