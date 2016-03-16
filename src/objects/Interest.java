25f911f5ed94fd77c42c07f12c52e99a1018ca6d
Nick Coriale <Nick Coriale>
2016-03-16 13:49:19 -0400

    Started creating the classes involving interests

---
+++ src/objects/Interest.java
@@ -0,0 +1,17 @@
+package objects;
+
+/**
+ * Created by nickj_000 on 3/16/2016.
+ */
+public class Interest {
+
+    private String name;
+    private String Description;
+    private String Category;
+
+    public Interest(String name, String description, String category) {
+        this.name = name;
+        Description = description;
+        Category = category;
+    }
+}
---
+++ src/objects/InterestsHelper.java
@@ -0,0 +1,27 @@
+package objects;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nickj_000 on 3/16/2016.
+ */
+public class InterestsHelper {
+
+    public ArrayList<Interest> getUserInterests(User user){
+        ArrayList<Interest> interests = new ArrayList<>();
+
+        // TODO sql query
+
+        return interests;
+    }
+
+    public void addInterestToDB(Interest interest){
+
+        // TODO sql query
+    }
+
+    //public addInterestToUser(username, interest) : Boolean
+
+    //private addCategory(name) : None
+
+}
