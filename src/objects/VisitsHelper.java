65ec690c3b2dd0babe5ce24a4c7616b5dba86326
Jarryd Lee <jarrydrlee@gmail.com>
2016-03-22 20:29:23 -0400

    Update VisitsHelper.java

--- src/objects/VisitsHelper.java
+++ src/objects/VisitsHelper.java
@@ -9,7 +9,7 @@ public class VisitsHelper {
 
   public void createVisit(Connection conn, String visited, String vistor) {
     try {
-      String query = "INSERT INTO visit VALUES (" + visited + ", " + vistor + ", " + System.currentTimeMillis() + ")";
+      String query = "INSERT INTO visit VALUES (" + visited + ", " + vistor + ", " + System.currentTimeMillis() + ");";
 
       Statement stmt = conn.createStatement();
       stmt.execute(query);
@@ -20,7 +20,7 @@ public class VisitsHelper {
 
   public ArrayList<User> getVisitsForUser(Connection conn, User user) {
     try {
-      String query = "SELECT * FROM visit WHERE visitor = " + user.getUsername();
+      String query = "SELECT * FROM visit WHERE visitor = " + user.getUsername() + ";";
 
       Statement stmt = conn.createStatement();
       stmt.execute(query);
