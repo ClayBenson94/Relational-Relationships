cc81cc1c31c8417cb238cc38c61c5a01ddb6d01d
Nick Coriale <njc3006@rit.edu>
2016-03-27 00:19:24 -0400

    Added InterestCategoriesTalbe a class responsible for creating the table and adding new entires

---
+++ src/tables/InterestCategoriesTable.java
@@ -0,0 +1,41 @@
+package tables;
+
+import objects.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InterestCategoriesTable {
+
+  public static void createInterestCategoriesTable(Connection conn) {
+    try {
+      String query = "CREATE TABLE interest_categories("
+        + "category_name VARCHAR(20),"
+        + "PRIMARY KEY (category_name),"
+        + ");";
+
+      /**
+       * Create a query and execute
+       */
+      Statement stmt = conn.createStatement();
+      stmt.execute(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static boolean addInterestCategory(Connection conn, User user, String category) {
+    try {
+      String query = "INSERT INTO interest_categories "
+        + "VALUES (category_name=\'" + category
+        + "\');";
+
+      Statement stmt = conn.createStatement();
+      return stmt.execute(query);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+}
