0a2e782aee26b510d824dd22a07a47126282f99b
Nick Coriale <njc3006@rit.edu>
2016-03-29 11:36:39 -0400

    Fixed a quote issue in a where clause, and implemented getVisitsForUser, decided visit class will make saving information in memory for display easier

--- src/tables/VisitTable.java
+++ src/tables/VisitTable.java
@@ -1,8 +1,10 @@
 package tables;
 
 import objects.User;
+import objects.Visit;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -42,16 +44,20 @@ public class VisitTable {
     }
   }
 
-  public ArrayList<User> getVisitsForUser(Connection conn, User user) {
+  public ArrayList<Visit> getVisitsForUser(Connection conn, User user) {
+    ArrayList<Visit> userVisits = new ArrayList<>();
     try {
-      String query = "SELECT * FROM visit WHERE visitor = " + user.getUsername() + ";";
+      String query = "SELECT * FROM visit WHERE visitor = \'" + user.getUsername() + "\';";
 
       Statement stmt = conn.createStatement();
-      stmt.execute(query);
+      ResultSet resultSet = stmt.executeQuery(query);
+      while (resultSet.next()) {
+        Visit visit = new Visit(resultSet.getString("visitor"), resultSet.getLong("timestamp"));
+        userVisits.add(visit);
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
-    // TODO Make this return an actual array list
-    return null;
+    return userVisits;
   }
 }
