c002a33dd2321c3d59e360eb7c2ddf1182de3392
ClayBenson94 <cmb3602@g.rit.edu>
2016-04-04 13:25:43 -0400

    Made basic SQL helper which wraps execute() and executeQuery()

---
+++ src/helpers/SQLHelper.java
@@ -0,0 +1,35 @@
+package helpers;
+
+import objects.Interest;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Clay on 4/4/2016.
+ */
+public class SQLHelper {
+
+    public static boolean execute(Connection conn, String query) {
+        try {
+            Statement stmt = conn.createStatement();
+            return stmt.execute(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static ResultSet executeQuery(Connection conn, String query) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery(query);
+            return resultSet;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
