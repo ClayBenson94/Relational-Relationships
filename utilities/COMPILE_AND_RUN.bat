80a913ff4bc01548c4df3f91fad7f4415219d68a
ClayBenson94 <cmb3602@g.rit.edu>
2016-04-10 15:49:57 -0400

    Edited COMPILE_AND_RUN scripts to prompt the user for using the -n flag

--- utilities/COMPILE_AND_RUN.bat
+++ utilities/COMPILE_AND_RUN.bat
@@ -1,5 +1,11 @@
+@echo off
 cd ..
 rmdir /s /q	"out"
 mkdir "out"
 javac -Xlint:unchecked -d "out" -cp "lib\forms_rt.jar;lib\h2-1.4.191.jar" src\helpers\*.java src\objects\*.java src\tables\*.java src\ui\*.java
-java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController
+
+echo.
+echo.
+set /P c=Generate Fresh Database[Y/N]?
+if /I "%c%" EQU "Y" java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController -n
+if /I "%c%" EQU "N" java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController
--- utilities/COMPILE_AND_RUN.sh
+++ utilities/COMPILE_AND_RUN.sh
@@ -3,4 +3,12 @@ cd ..
 rm -rf "out"
 mkdir "out"
 javac -Xlint:unchecked -d "out" -cp "lib/forms_rt.jar:lib/h2-1.4.191.jar" src/helpers/*.java src/objects/*.java src/tables/*.java src/ui/*.java
-java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController
+
+read -p "Generate Fresh Database[Y/N]? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController -n
+else
+	java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController
+fi
