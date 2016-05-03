8a363619d095ccd5dab9a7654aea747e1174e99c
Jarryd Lee <jarrydrlee@gmail.com>
2016-05-03 10:16:58 -0400

    Visit generation

--- src/helpers/VisitsGen.java
+++ src/helpers/VisitsGen.java
@@ -1,4 +1,43 @@
 package helpers;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import objects.RelationshipController;
+
 public class VisitsGen {
+
+    public void genereateVisits(int count) {
+        String selectUserQuery = "SELECT username FROM user";
+        String insertVisitQuery = "INSERT INTO visit(visited, visitor, timestamp) VALUES %s;";
+        Connection conn = RelationshipController.getConnection();
+        ResultSet resultSet = SQLHelper.executeQuery(conn, selectUserQuery);
+        ArrayList<String> usernames = new ArrayList<>();
+
+        try {
+            while (resultSet.next()) {
+                usernames.add(resultSet.getString("username"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Random random = new Random();
+        int first;
+        int second;
+        int usernameLength = usernames.size();
+        ArrayList<String> visitsToInsert = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            first = random.nextInt(usernameLength);
+            second = random.nextInt(usernameLength);
+            long timestamp = System.currentTimeMillis();
+            visitsToInsert.add("(\'" + usernames.get(first) + "\',\'" + usernames.get(second) + "\'," + timestamp + ")");
+        }
+        insertVisitQuery = String.format(insertVisitQuery, String.join(",", visitsToInsert));
+        SQLHelper.execute(conn, insertVisitQuery);
+    }
 }
