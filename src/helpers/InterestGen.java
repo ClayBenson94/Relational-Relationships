bb5b1eafd182f52032f13ae6dafba36da029708a
Nicholas Coriale <njc3006@rit.edu>
2016-05-09 22:09:47 -0400

    Modifed user interest generation to use 1 sql statement and not throw…

--- src/helpers/InterestGen.java
+++ src/helpers/InterestGen.java
@@ -1,34 +1,56 @@
 package helpers;
 
-
+import objects.Interest;
 import objects.RelationshipController;
 import objects.User;
+import tables.InterestTable;
 import tables.UserInterestsTable;
 
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Random;
 
+/**
+ * A class for generating user_interest rows in the database
+ */
 public class InterestGen {
 
+    /**
+     * Generate 2 to 30 interests for every passed user
+     * @param users - an ArrayList of users to add interests to
+     */
     public void generateUserInterests(ArrayList<User> users) {
+        String insertUserInterestQuery = "INSERT INTO user_interests(username, interest) VALUES %s;";
+        Connection conn = RelationshipController.getConnection();
+
+        // Get all the interests that are currently in the database
+        ArrayList<Interest> interests = InterestTable.getInterests(conn);
+
+        ArrayList<String> userInterestsToInsert = new ArrayList<>();
+
         for (User u : users) {
+            // Create a copy of the ArrayList representing interests in the db so we can manipulate it
+            ArrayList<Interest> copyOfInterests = new ArrayList<>(interests);
+
+            // Get the interests the user already has, if any
+            ArrayList<Interest> userInterests = UserInterestsTable.getUserInterests(conn, u.getUsername());
+
+            // Remove all interests from the copy that the user already has
+            copyOfInterests.removeAll(userInterests);
+
+            // Add 2 to 30 interests to the user
             for (int i = RandomNumberHelper.randBetween(2, 30); i > 0; i--) {
-                String query = "insert into user_interests values(\'" + u.getUsername() +
-                        "\', (select interest_name from interests ORDER BY RAND() LIMIT 1));";
-                Statement stmt = null;
-                try {
-                    stmt = RelationshipController.getConnection().createStatement();
-                    stmt.executeUpdate(query);
-                } catch (SQLException e) {
-                    if (e.getSQLState().equals("23505")) {
-                        // user already has that interest, do nothing
-                    } else {
-                        e.printStackTrace();
-                    }
-                }
+
+                // Get a random interest the user does not already have
+                int randomInterestIndex = RandomNumberHelper.randBetween(0, copyOfInterests.size() -1);
+                Interest interestToAdd = copyOfInterests.get(randomInterestIndex);
+
+                // Remove that interest from the copy list to prevent adding it again later
+                copyOfInterests.remove(interestToAdd);
+
+                userInterestsToInsert.add("(\'" + u.getUsername() + "\',\'" + interestToAdd.getName() + "\')");
             }
         }
+        insertUserInterestQuery = String.format(insertUserInterestQuery, String.join(",", userInterestsToInsert));
+        SQLHelper.execute(conn, insertUserInterestQuery);
     }
 }
