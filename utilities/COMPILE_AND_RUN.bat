9b6f588f629d516334b4087c91d33a29fdea0fc0
ClayBenson94 <cmb3602@g.rit.edu>
2016-05-12 12:50:47 -0400

    Use -g flag in compilation scripts

--- utilities/COMPILE_AND_RUN.bat
+++ utilities/COMPILE_AND_RUN.bat
@@ -7,5 +7,5 @@ javac -Xlint:unchecked -d "out" -cp "lib\forms_rt.jar;lib\h2-1.4.191.jar" src\he
 echo.
 echo.
 set /P c=Generate Fresh Database[Y/N]?
-if /I "%c%" EQU "Y" java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController -n
+if /I "%c%" EQU "Y" java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController -n -g 1000 500
 if /I "%c%" EQU "N" java -classpath "out;lib\h2-1.4.191.jar;lib\forms_rt.jar" objects.RelationshipController
--- utilities/COMPILE_AND_RUN.sh
+++ utilities/COMPILE_AND_RUN.sh
@@ -8,7 +8,7 @@ read -p "Generate Fresh Database[Y/N]? " -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController -n
+    java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController -n -g 1000 500
 else
 	java -classpath "out:lib/h2-1.4.191.jar:lib/forms_rt.jar" objects.RelationshipController
 fi
